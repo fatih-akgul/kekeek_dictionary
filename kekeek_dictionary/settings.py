bc58eabd2077d9908715049e1d6f032cec3096fb
Fatih Akgul <akguls@gmail.com>
2019-10-22 15:44:32 -0400

    Use postgres instead of sqlite

--- kekeek_dictionary/settings.py
+++ kekeek_dictionary/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'kekeek_dictionary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USERNAME'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
