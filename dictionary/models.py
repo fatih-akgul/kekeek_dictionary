4ad96390b421f6435042771c6d41853b5d0475aa
Fatih Akgul <akguls@gmail.com>
2019-10-23 10:51:01 -0400

    Add pages app

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class Dictionary(models.Model):
+    identifier = models.CharField(max_length=256, db_index=True)
+    name = models.CharField(max_length=256)
+    short_description = models.CharField(max_length=1024)
+    long_description = models.CharField(max_length=2048)
+    url = models.CharField(max_length=512, blank=True)
+    language_from = models.CharField(max_length=64, db_index=True)
+    language_to = models.CharField(max_length=64, db_index=True)
+    # Date fields:
+    date_created = models.DateTimeField(auto_now_add=True, db_index=True)
+    date_modified = models.DateTimeField(auto_now=True, db_index=True)
+
+
+class Word(models.Model):
+    dictionary = models.ForeignKey(Dictionary, on_delete=models.CASCADE)
+    word = models.CharField(max_length=256, db_index=True)
+    word_simplified = models.CharField(max_length=256, db_index=True)
+    details = JSONField()
+    # Date fields:
+    date_created = models.DateTimeField(auto_now_add=True, db_index=True)
+    date_modified = models.DateTimeField(auto_now=True, db_index=True)
